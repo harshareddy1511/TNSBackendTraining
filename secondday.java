e105df41991ef80bc3d2bad26e80733f7fcecbfa
harshareddy1511 <harshareddyvardhan15112003@gmail.com>
2025-08-05 21:36:23 +0530

    Initial commit

---
+++ secondday.java
@@ -0,0 +1,9 @@
+// Typecasting in Java 
+// Implicit Typecasting Example
+public class secondday {
+    public static void main(String[] args) {
+        short x = 10;
+        int y = x;
+        System.out.println("The Value of Y is: "+y);
+    }
+}
